d16b6328265fd708ae3c42c4e8353bc663dcc3c4
harybott <harybott@gmail.com>
2016-11-07 16:00:03 +0800

    First Commit

---
+++ src/scripts/TestWoodCutting.java
@@ -0,0 +1,24 @@
+package scripts;
+
+
+import org.tribot.api2007.Inventory;
+import org.tribot.script.Script;
+import org.tribot.script.ScriptManifest;
+import org.tribot.api2007.util.*;
+
+/**
+ * Created by Administrator on 2016/11/3.
+ */
+@ScriptManifest(authors = "Zwww",name = "TestWoodCutting",category = "Test")
+public class TestWoodCutting extends Script {
+    //Inventory 我的程序 的是
+    public static final int num
+
+
+    public void run() {
+        return num;
+
+        ns
+
+    }
+}
