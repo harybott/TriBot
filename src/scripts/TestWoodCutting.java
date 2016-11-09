d170807ad2a6360811f47a4e95edd0b54bf60f09
harybott <harybott@gmail.com>
2016-11-09 10:46:46 +0800

    修改

--- src/scripts/TestWoodCutting.java
+++ src/scripts/TestWoodCutting.java
@@ -1,21 +1,33 @@
 package scripts;
 
 
+import org.tribot.api.input.Keyboard;
+import org.tribot.api.input.Mouse;
 import org.tribot.script.Script;
 import org.tribot.script.ScriptManifest;
 
 /**
  * Created by Administrator on 2016/11/3.
  */
+
 @ScriptManifest(authors = "Zwww", name = "TestWoodCutting", category = "Test")
 public class TestWoodCutting extends Script {
 
     //Inventory 我的程序 的是
-    private static final int num;
+    private static final int num = 0;
 
 
     public static int getNum() {
-        return num;
+        System.out.println("-----------Zwww.com-----------  = " + num + ", " + "当前类 = TestWoodCutting.getNum()");
+        System.out.println("-----------Zwww.com----------- dir = " + num + ", " + "当前类 = TestWoodCutting.getNum()");
+        System.out.println("-----------Zwww.com-----------  = " + num + ", " + "当前类 = TestWoodCutting.getNum()");
+
+        Keyboard.getSpeed();
+        Mouse.getSpeed();
+        System.out.println("dir dir dir " + dir + num);
+
+
+        return 0;
     }
 
     public void run() {
